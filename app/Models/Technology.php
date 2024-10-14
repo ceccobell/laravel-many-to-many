c6bd3bd60d30d867f69c985b26a33bc0a9e3ec21
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-14 16:08:33 +0200

    [UPd] created model Technology

---
+++ app/Models/Technology.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Technology extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'name',
+        'slug',
+    ];
+
+    public static function GenerateSlug($title){
+        return Str::slug($title, '-');
+    }
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
+}
